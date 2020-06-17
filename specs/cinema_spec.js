ac1b4efde49ad77877e94511d371a73cd119e22d
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-17 15:19:06 +0100

    Extension completed

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -40,7 +40,7 @@ describe('Cinema', function () {
   });
 
   it('should be able to filter films by genre', function () {
-    const actual = cinema.getFilmsByGenre('drama');
+    const actual = cinema.filmsByProperty('genre', 'drama');
     assert.deepStrictEqual(actual, [moonlight, trainspotting])
   });
 
